ccfc4d8e0d7926ba478f618f1d1d6a68f9f16353
MatthewW05 <wongmatthew6767@gmail.com>
2023-11-21 23:59:38 -0500

    Sixth Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -109,8 +109,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'rt.scheduling.automailer@gmail.com'
+EMAIL_HOST_PASSWORD = 'bfpv cbya mogv qicj'
 
 
 # Internationalization
